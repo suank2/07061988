0c4c1b56caca0cb8dc7878057ac6a65e2ea2dbdf
USER <USER@USER-PC>
2019-03-10 15:37:26 +0530

    Anonymous

--- AnonymousDemo/src/com/inner/demo/Food.java
+++ AnonymousDemo/src/com/inner/demo/Food.java
@@ -4,7 +4,8 @@ public class Food {
 	
 	//Food has one instance variable declared as type Popcorn
 	//Food has no methods
-	//Popcorn reference variable does not refer to an instance of Popcorn, but to an instance of an anonymous subclass of Popcorn
+	//Popcorn reference variable does not refer to an instance of Popcorn,
+	//but to an instance of an anonymous subclass of Popcorn
 	
 	Popcorn p = new Popcorn(){		
 		public void pop(){

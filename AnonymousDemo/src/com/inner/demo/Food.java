9d9aec941464ade8345d5a7d590e69231a73e52f
USER <USER@USER-PC>
2019-03-10 15:36:29 +0530

    Anonymous

--- AnonymousDemo/src/com/inner/demo/Food.java
+++ AnonymousDemo/src/com/inner/demo/Food.java
@@ -3,6 +3,9 @@ package com.inner.demo;
 public class Food {
 	
 	//Food has one instance variable declared as type Popcorn
+	//Food has no methods
+	//Popcorn reference variable does not refer to an instance of Popcorn, but to an instance of an anonymous subclass of Popcorn
+	
 	Popcorn p = new Popcorn(){		
 		public void pop(){
 			System.out.println("anonymous");
